63e4aba2d896eb3829dd8b46b69ecd268ce2bc32
Ouly <1375288745@qq.com>
2022-02-17 00:40:35 +0800

    服务器端 sql 引用和代码编写

---
+++ Server/Server/Server/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server.Server
+{
+    public class Server
+    {
+        private IPEndPoint _ipEndPoint;
+        private Socket serverSocket;
+        
+        public Server(){}
+
+        public Server(string ipStr, int port)
+        {
+            SetIpAndPort(ipStr, port);
+        }
+
+        public void SetIpAndPort(string ipStr, int port)
+        {
+            _ipEndPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);
+        }
+
+        public void Start()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+            serverSocket.Bind(_ipEndPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+        }
+    }
+    
+}
